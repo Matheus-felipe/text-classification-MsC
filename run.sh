df8d2808957ce87746e0a64a6c0aecac57c14206
Denini Gabriel Silva <dgs@linuxapps02.cin.ufpe.br>
2022-05-25 10:31:28 -0300

    update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python3 experimentos3_0.py FV &> fv.log &
-python3 experimentos3_0.py TF_IDF &> tf_idf.log &
-python3 experimentos3_0.py W2V &> w2v.log &
-python3 experimentos3_0.py Features_Textuais &> feat.log &
-python3 experimentos3_0.py TF_IDF_Features_Textuais &> tf.log &
-python3 experimentos3_0.py FV_Features_Textuais &> fv_f.log &
-python3 experimentos3_0.py W2V_Features_Textuais &> w2v_f.log &
-python3 experimentos3_0.py ALL &> all.log &
+python3 -u experimentos3_0.py FV &> logs/fv.log &
+python3 -u experimentos3_0.py TF_IDF &> logs/tf_idf.log &
+python3 -u experimentos3_0.py W2V &> logs/w2v.log &
+python3 -u experimentos3_0.py Features_Textuais &> logs/feat.log &
+python3 -u experimentos3_0.py TF_IDF_Features_Textuais &> logs/tf.log &
+python3 -u experimentos3_0.py FV_Features_Textuais &> logs/fv_f.log &
+python3 -u experimentos3_0.py W2V_Features_Textuais &> logs/w2v_f.log &
+python3 -u experimentos3_0.py ALL &> logs/all.log &
